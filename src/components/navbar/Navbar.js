e1a7e73989a525f8cfab61ff11f89c524e0198f1
NikhilDhiman <nikhil.dhiman7973@gmail.com>
2024-10-17 09:58:14 +0530

    minor changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -32,13 +32,13 @@ const Navbar = () => {
       <nav className={`nav ${navbarAffix ? 'affix' : ''}`}>
         <div className="container">
           <div className="logo">
-            <a href="#">Your Logo</a>
+            <a href="#">Easy Job</a>
           </div>
           <div id="mainListDiv" className={`main_list ${menuActive ? 'show_list' : ''}`}>
             <ul className="navlinks">
-              <li><a href="#">About</a></li>
-              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Find companies</a></li>
               <li><a href="#">Services</a></li>
+              <li><a href="#">About</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
           </div>
