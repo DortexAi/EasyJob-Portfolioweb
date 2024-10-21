97184360b3c30ef11bec35688e222640c63b74cb
NikhilDhiman <nikhil.dhiman7973@gmail.com>
2024-10-21 11:13:53 +0530

    done

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import Home from './pages/Home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
